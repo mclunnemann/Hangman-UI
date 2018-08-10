14be6356d665533c6a24fb2718c6e8202fc1de1d
mclunnemann <mclunnemann@gmail.com>
2018-08-10 15:19:47 -0400

    Add files via upload

---
+++ HangmanWithUI/HangmanWithUI/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace HangmanWithUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayWord = new System.Windows.Forms.Label();
+            this.tbxGuess = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.displayWrongGuess = new System.Windows.Forms.Label();
+            this.lblHangman = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // displayWord
+            // 
+            this.displayWord.AutoSize = true;
+            this.displayWord.BackColor = System.Drawing.SystemColors.Control;
+            this.displayWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayWord.Location = new System.Drawing.Point(130, 503);
+            this.displayWord.Name = "displayWord";
+            this.displayWord.Size = new System.Drawing.Size(324, 25);
+            this.displayWord.TabIndex = 0;
+            this.displayWord.Text = "__________________________";
+            // 
+            // tbxGuess
+            // 
+            this.tbxGuess.Location = new System.Drawing.Point(39, 508);
+            this.tbxGuess.MaxLength = 1;
+            this.tbxGuess.Name = "tbxGuess";
+            this.tbxGuess.Size = new System.Drawing.Size(59, 20);
+            this.tbxGuess.TabIndex = 1;
+            this.tbxGuess.TextChanged += new System.EventHandler(this.tbxGuess_TextChanged);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(39, 539);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(58, 25);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // displayWrongGuess
+            // 
+            this.displayWrongGuess.AutoSize = true;
+            this.displayWrongGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayWrongGuess.Location = new System.Drawing.Point(130, 536);
+            this.displayWrongGuess.Name = "displayWrongGuess";
+            this.displayWrongGuess.Size = new System.Drawing.Size(324, 25);
+            this.displayWrongGuess.TabIndex = 3;
+            this.displayWrongGuess.Text = "__________________________";
+            // 
+            // lblHangman
+            // 
+            this.lblHangman.AutoSize = true;
+            this.lblHangman.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblHangman.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHangman.Location = new System.Drawing.Point(203, 106);
+            this.lblHangman.Name = "lblHangman";
+            this.lblHangman.Size = new System.Drawing.Size(121, 304);
+            this.lblHangman.TabIndex = 4;
+            this.lblHangman.Text = " @\r\n \\|/\r\n  |\r\n / \\";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Location = new System.Drawing.Point(135, 92);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(260, 334);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 608);
+            this.Controls.Add(this.lblHangman);
+            this.Controls.Add(this.displayWrongGuess);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.tbxGuess);
+            this.Controls.Add(this.displayWord);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label displayWord;
+        private System.Windows.Forms.TextBox tbxGuess;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label displayWrongGuess;
+        private System.Windows.Forms.Label lblHangman;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ HangmanWithUI/HangmanWithUI/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HangmanWithUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string word;
+        string[] chars;
+        string[] word2;
+        ArrayList wrongGuess = new ArrayList();
+        int timesFailed = 0;
+
+        private void Clear()
+        {
+            wrongGuess.Clear();
+            displayWord.Text = "";
+            displayWrongGuess.Text = "";
+            lblHangman.Text = "";
+            timesFailed = 0;
+            word = "";            
+
+        }
+        private void loadForm()
+        {            
+            lblHangman.Text = "";
+            string URL1 = "http://www-01.sil.org/linguistics/wordlists/english/wordlist/wordsEn.txt";
+            ArrayList input1 = new ArrayList();
+
+            WebResponse response1;
+            WebRequest request1 = HttpWebRequest.Create(URL1);
+            response1 = request1.GetResponse();
+
+            StreamReader sr1 = new StreamReader(response1.GetResponseStream());
+
+            while (!sr1.EndOfStream)
+            {
+                input1.Add(sr1.ReadLine());
+            }
+
+            sr1.Close();
+            Random rnd = new Random();
+
+            int rand = rnd.Next(0, input1.Count);
+
+            word = (string)input1[rand];
+            chars = new string[word.Length];
+            word2 = new string[word.Length];
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                chars[i] = word.Substring(i, 1);
+                word2[i] = "_ ";
+                displayWord.Text += word2[i];
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+            loadForm();
+        }
+
+        private void ClickEnter()
+        {
+            string guess = tbxGuess.Text;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (word.Contains(guess))
+                {
+                    if (chars[i] == guess)
+                    {                       
+                        word2[i] = guess + " ";                       
+                    }
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+                        wrongGuess.Add(guess);
+                    }                                                                                   
+                }
+            }
+
+            displayWord.Text = "";
+            for (int i = 0; i < word2.Length; i++)
+            {
+                displayWord.Text += word2[i];
+            }
+
+            displayWrongGuess.Text = "";
+            for (int i = 0; i < wrongGuess.Count; i++)
+            {
+                displayWrongGuess.Text += wrongGuess[i] + " ";
+            }
+
+            if (!word.Contains(guess))
+            {
+                timesFailed++;
+                if (timesFailed == 1)
+                {
+                    lblHangman.Text = " @";
+                }
+                if (timesFailed == 2)
+                {
+                    lblHangman.Text += "\n \\";
+                }
+                if (timesFailed == 3)
+                {
+                    lblHangman.Text += "|";
+                }
+                if (timesFailed == 4)
+                {
+                    lblHangman.Text += "/";
+                }
+                if (timesFailed == 5)
+                {
+                    lblHangman.Text += "\n  |";
+                }
+                if (timesFailed == 6)
+                {
+                    lblHangman.Text += "\n /";
+                }
+                if (timesFailed == 7)
+                {
+                    lblHangman.Text += " \\";
+
+                    if (MessageBox.Show("You did not guess the correct letters. The word was " + word + ". \nDo you wish to try again?", "Hangman: Bummer", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        Clear();
+                        loadForm();
+                    }
+                    else
+                    {
+                        Application.Exit();
+                    }
+                }
+            }
+            int count = 0;
+            for (int i =0; i < word2.Length; i++)
+            {
+                if (word2[i].Contains("_"))
+                {
+                    count++;
+                }
+            }
+            if(count == 0)
+            {
+                if (MessageBox.Show("You guessed all correct letters. The word was " + word + ". \nDo you wish to try again?", "Hangman: Congrats!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    Clear();
+                    loadForm();                    
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+            
+            tbxGuess.Clear();
+            tbxGuess.Focus();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            ClickEnter();
+        }
+
+        private void tbxGuess_TextChanged(object sender, EventArgs e)
+        {
+            btnEnter.Focus();
+        }
+    }
+}
+
+
